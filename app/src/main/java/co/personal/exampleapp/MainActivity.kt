b360d165e824801bcd2cb47b89ca78d54b695c55
Enrique Morales <bluesmusic8@gmail.com>
2021-03-13 05:45:58 -0500

    practicando migration room

--- app/src/main/java/co/personal/exampleapp/MainActivity.kt
+++ app/src/main/java/co/personal/exampleapp/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        database = Room.databaseBuilder(this, TasksDatabase::class.java, "tasks-db").build()
+        database = Room.databaseBuilder(this, TasksDatabase::class.java, "tasks-db")
+            .build()
 
         GlobalScope.launch {
 
